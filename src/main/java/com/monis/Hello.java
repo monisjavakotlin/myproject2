15db997b0a9a279558b0d165ede896f69d4fd8a5
monis <monis5362413@gmail.com>
2019-08-20 09:43:05 +0800

    variable,data type

--- src/main/java/com/monis/Hello.java
+++ src/main/java/com/monis/Hello.java
@@ -5,5 +5,18 @@ public class Hello {
 //        System.out.println("Hello monis");
         Person person = new Person();
         person.hello();
+        //基本資料型態
+        int age = 19;
+        char c = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        double height = 1.7;
+        boolean adult = true;
+        boolean enroll = false;
+        //參照資料型態 - wrapper class
+        Integer age2 = 29;
+        Character c2 = 'B';
+        String name = "Monis";
+
     }
 }
--- src/main/kotlin/com/monis/Hello.kt
+++ src/main/kotlin/com/monis/Hello.kt
@@ -5,9 +5,17 @@ fun main() {
 //    Human().hello()
     val human = Human()
     human.hello()
+    //資料型態都是物件
+    val age = 19  //不可變
+    val name = "Monis"
+    var weight = 66.5 //可變
+    var name2: String
+    name2 = "monis"
+
+
 }
 
-class Human(){
+class Human() {
     fun hello() {
         println("Hello kotlin")
     }
